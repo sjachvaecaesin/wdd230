302d6b43a710cb5b299aea23d38891209c20bdd3
sjachvaecaesin <booglyboo544@gmail.com>
2024-02-04 01:45:26 -0500

    comment

--- chamber/scripts/modified.js
+++ chamber/scripts/modified.js
@@ -1,3 +1,9 @@
-document.querySelector("#year").textContent = new Date().getFullYear();
-
 document.querySelector("#lastModified").textContent = document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
