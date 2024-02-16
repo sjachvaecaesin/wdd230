d2b1ed86fc96beeca47642a751d587ca12f614b4
sjachvaecaesin <booglyboo544@gmail.com>
2024-02-16 00:40:01 -0500

    ;alksdfj

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,8 +4,13 @@ document.querySelector("#lastModified").textContent = `Last Modified: ${document
 
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
+const heroLabel = document.querySelector(".herolabel");
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
+	heroLabel.style.top = "";
+	if (hamButton.classList.contains('open')) {
+		heroLabel.style.top = "350px";
+	};
 });
