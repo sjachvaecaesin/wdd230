6bbb044093efe24347b2c95316b4b9842e332b09
sjachvaecaesin <booglyboo544@gmail.com>
2024-03-08 17:38:17 -0500

    asfd

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,19 @@
 const baseURL = "https://sjachvaecaesin.github.io/wdd230/";
 const linksURL = "https://sjachvaecaesin.github.io/wdd230/data/links.json";
+const listElement = document.querySelector("#list");
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data.lessons);
+}
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        let list = document.createElement("li");
+        let att = document.createElement("a");
+        console.log(week);
+    });
 }
 
 getLinks();
